db26a006df3cc24a57b27cc35e6293688b86e0bd
hedgerwang <hedgerwang@f02cbbde-50c2-11de-ac45-ff9ca5716a9b>
2009-06-04 18:18:50 +0000

    * Add onPreviewSuccess and onPreviewFail handlers. * Add fileSize attribute. * Add more JSDoc.

--- trunk/imageuploadpreview.js
+++ trunk/imageuploadpreview.js
@@ -10,9 +10,11 @@
 /**
  * @constructor
  * @param {HTMLInputElement|String} input
+ * @param {Function?} opt_onSuccess
+ * @param {Function?} opt_onFail
  */
-function ImageUploadPreview(input) {
-  this.construct(input);
+function ImageUploadPreview(input, opt_onSuccess, opt_onFail) {
+  this.construct(input, opt_onSuccess, opt_onFail);
 }
 
 /**
@@ -41,6 +43,20 @@ ImageUploadPreview.prototype.input_ = null;
  * @private
  */
 ImageUploadPreview.prototype.onChangeHandler_ = null;
+
+
+/**
+ * @type {Function}
+ * @private
+ */
+ImageUploadPreview.prototype.onPreviewSuccessHandler_ = null;
+
+
+/**
+ * @type {Function}
+ * @private
+ */
+ImageUploadPreview.prototype.onPreviewFailHandler_ = null;
 
 
 /**
@@ -73,13 +89,18 @@ ImageUploadPreview.prototype.maxHeight_ = 200;
 
 /**
  * @param {HTMLInputElement|String} input
+ * @param {Function?} opt_onSuccess
+ * @param {Function?} opt_onFail 
  * @public
  */
-ImageUploadPreview.prototype.construct = function(input) {
+ImageUploadPreview.prototype.construct =
+function(input, opt_onSuccess, opt_onFail) {
   if (typeof input == 'string') {
     input = document.getElementById(input);
   }
 
+  this.onPreviewFailHandler_ = opt_onFail;
+  this.onPreviewSuccessHandler_ = opt_onSuccess;
   this.input_ = input;
   this.bindEvents_();
   this.image_ = this.createImage_();
@@ -91,6 +112,10 @@ ImageUploadPreview.prototype.construct = function(input) {
  */
 ImageUploadPreview.prototype.dispose = function() {
   var fn = this.onChangeHandler_;
+
+  // Already disposed.
+  if (!fn) return;
+
   var el = this.input_;
 
   if (el.addEventListener) {
@@ -110,19 +135,21 @@ ImageUploadPreview.prototype.dispose = function() {
 ImageUploadPreview.prototype.preview = function() {
   var that = this;
 
-  var onLoad = function(src) {
+  var onLoad = function(imgInfo) {
     // Do thing now, maybe do something later.
+    that.maybeCallFunction_(that.onPreviewSuccessHandler_, imgInfo);
   };
 
   var onError = function(src) {
     if (!tryLoad()) {
       that.showEmptyImage_();
+      that.maybeCallFunction_(that.onPreviewFailHandler_, src);
     }
   };
 
   var loadMethods = [
     this.maybeShowImageWithDataUri_,
-    this.maybeShowImageByLocalPath_
+    this.maybeShowImageByPath_
   ];
 
   var tryLoad = function() {
@@ -256,6 +283,17 @@ ImageUploadPreview.prototype.showImage_ = function(src, w, h) {
 
 
 /**
+ * @param {Function?} fn
+ * @param {Object?} var_args
+ */
+ImageUploadPreview.prototype.maybeCallFunction_ = function(fn, var_args) {
+  if (typeof fn != 'function') return;
+  var_args = Array.prototype.slice.call(arguments, 1);
+  fn.apply(this, var_args);
+};
+
+
+/**
  * Note: Only Firefox 3 can do file.getAsDataURL() by 6/1/2009.
  * See {@link https://developer.mozilla.org/En/NsIDOMFile}.
  * @private
@@ -267,7 +305,9 @@ function(opt_onload, opt_onerror) {
   var el = this.input_;
   var file = el.files && el.files[0];
   var src;
+  var fileName = el.value;
 
+  // Check if we can access the serialized file via getAsDataURL().
   if ((file && file.getAsDataURL) &&
       (src = file.getAsDataURL()) &&
       (ImageUploadPreview.BASE64_IMG_URL_PATTERN.test(src))) {
@@ -275,42 +315,53 @@ function(opt_onload, opt_onerror) {
     var that = this;
     var img = this.image_;
 
-    if ('naturalWidth' in this.image_) {
 
+    if ('naturalWidth' in this.image_) {
+      // Firefox has naturalWidth.
       this.image_.src = src;
+
       setTimeout(function() {
         that.showImage_(src, img.naturalWidth, img.naturalHeight);
+        that.maybeCallFunction_(opt_onload, {
+          width: img.naturalWidth,
+          height: img.naturalHeight,
+          fileName : fileName,
+          fileSize: el.files[0].fileSize
+        });
       }, 10);
 
     } else {
       // Use default CSS max-width / max-height to render the size.
       that.showImage_(src, -1, -1);
-    }
 
-    if (opt_onload) {
-      opt_onload.call(this, src);
+      this.maybeCallFunction_(opt_onload, {
+        fileName : fileName,
+        width: img.offsetWidth,
+        height: img.offsetHeight,
+        fileSize: el.files[0].fileSize
+      });
     }
-
   } else {
-    if (opt_onerror) {
-      opt_onerror.call(this);
-    }
+    this.maybeCallFunction_(opt_onerror, fileName);
   }
 };
 
 
 /**
  * Note: IE6 ~ IE8 can access image with local path. By 6/1/2009.
+ *       However, this may still not work if the security setting changes.
  * @private
  * @param {Function?} opt_onload
  * @param {Function?} opt_onerror
  */
-ImageUploadPreview.prototype.maybeShowImageByLocalPath_ =
+ImageUploadPreview.prototype.maybeShowImageByPath_ =
 function(opt_onload, opt_onerror) {
+
   var that = this;
   var el = this.input_;
   var img = new Image();
   var timer;
+  var fileName = el.value.split(/[\\\/]/ig).pop();
 
   var dispose = function() {
     if (timer) {
@@ -322,33 +373,44 @@ function(opt_onload, opt_onerror) {
     dispose = null;
     img = null;
     that = null;
-    checkComplete = null;
+    checkIsComplete = null;
     handleError = null;
     handleComplete = null;
   };
 
+  // Handle the case whether the File is not a image file or the path is not a
+  // valid path to access.
   var handleError = function() {
-    if (opt_onerror) {
-      opt_onerror.call(that);
-    }
+    that.maybeCallFunction_(opt_onerror, el.value);
     dispose();
   };
 
   var handleComplete = function() {
-    that.showImage_(img.src, img.width, img.height);
-    if (opt_onload) {
-      opt_onload.call(that, img.src);
-    }
+    var w = img.width;
+    var h = img.height;
+    that.showImage_(img.src, w, h);
+    that.maybeCallFunction_(opt_onload, {
+      src:fileName,
+      width: w,
+      height: h,
+      fileSize: img.fileSize // Note that FileSize is an IE's only attribute.
+    });
     dispose();
   };
 
-  var checkComplete = function() {
-    var type = window.event && window.event.type;
+  var checkIsComplete = function(e) {
+    e = e || window.event;
+    var type = e && e.type;
 
     if (type == 'error') {
+      // If the onError event is called.
       handleError();
     } else  if (img.complete || type == 'load') {
+      // Sometimes IE does not fire "onload" event if the image was cahced.
+      // So we have to check the "complete" state to know whether it's ready.
       if (!img.width || !img.height) {
+        // Even it's not a real image, the onload event may still gets fired.
+        // Check if its width or height is 0. If true, it's not a real image.
         handleError();
       } else {
         handleComplete();
@@ -356,10 +418,14 @@ function(opt_onload, opt_onerror) {
     }
   };
 
-  img.onload = checkComplete;
-  img.onerror = checkComplete;
-  timer = window.setInterval(checkComplete, 50);
+  img.onload = checkIsComplete;
+  img.onerror = checkIsComplete;
+  timer = window.setInterval(checkIsComplete, 50);
+
+  // In IE, el.value us the full path of the file rather than just the fileName,
+  // and we'd test if we can load image from this path.
   img.src = el.value;
+
 };
 
 
